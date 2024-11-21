bc842cda935110385e6a8b97d577529d1335efbd
shazlinn <shazlinkay@gmail.com>
2024-11-21 13:51:48 +0800

    added search bar component

---
+++ src/app/(dashboard)/admin/manageUser3/page.tsx
@@ -0,0 +1,38 @@
+import AdminGuard from "./components/AdminGuard";
+import styles from "./styles/manageUser.module.scss";
+import UserList from "./components/UserList";
+import AddButton from "./components/addButton";
+import Search from "./components/SearchBar";
+
+export default async function ManageUserPage(props: {
+  searchParams?: Promise<{
+    query?: string;
+    page?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+  
+
+  return (
+    <AdminGuard isAdmin={true}>
+      <div className={styles.pageContainer}>
+        <div className={styles.contentContainer}>
+          <div className={styles.manageUserPage}>
+            <h1 style={{ fontWeight: "bold" }}>User Management</h1>
+            <div className={styles.searchAndAddContainer}>
+            <Search placeholder="Search..." />
+            <AddButton />
+        </div>
+
+            {/* <Search placeholder="Search..." />
+            <AddButton /> */}
+            <UserList query={query} currentPage={currentPage} />
+            
+          </div>
+        </div>
+      </div>
+    </AdminGuard>
+  );
+}
