e61918069b676f46d17f29957fbfd4a062b22ad3
shazlinn <shazlinkay@gmail.com>
2024-11-21 15:45:04 +0800

    changed login background

---
+++ src/app/(dashboard)/admin/manageUser3/components/Pagination.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import Link from 'next/link';
+import { usePathname, useSearchParams } from 'next/navigation';
+ 
+export default function Pagination({ totalPages }: { totalPages: number }) {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const currentPage = Number(searchParams.get('page')) || 1;
+ 
+  const createPageURL = (pageNumber: number | string) => {
+    const params = new URLSearchParams(searchParams);
+    params.set('page', pageNumber.toString());
+    return `${pathname}?${params.toString()}`;
+  };
+
+}
